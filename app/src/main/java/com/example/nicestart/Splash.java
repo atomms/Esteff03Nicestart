b834ed242aee25748ddef5f57090900a232ffed2
sindy nunez <sindy.nunez@educa.madrid.org>
2024-10-28 13:45:12 +0100

    commit

--- app/src/main/java/com/example/nicestart/Splash.java
+++ app/src/main/java/com/example/nicestart/Splash.java
@@ -1,14 +1,21 @@
 package com.example.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Splash extends AppCompatActivity {
 
@@ -20,6 +27,40 @@ public class Splash extends AppCompatActivity {
 
         //Metodo para arrancar Splash
         openApp();
+
+
+        //implements and starts animation
+        // objeto thunder sobre el cual aplicaremos la animación
+        ImageView thunder = findViewById(R.id.logo);
+
+        //creamos un objeto animación que incorpora la animación descrita en el xml y con el método
+        // startAnimation lo aplicamos al imageview del logo
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.blink);
+        thunder.startAnimation(myanim);
+
+
+        ImageView texto = (ImageView) findViewById(R.id.logo);
+        Animation animacion = AnimationUtils.loadAnimation(this,
+                R.anim.blink);
+        texto.startAnimation(animacion);
+
+
+        //    Glide for loading girls
+        ImageView mSea = findViewById(R.id.imagen);
+
+
+        Glide.with(this)
+               .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+//                .load(R.drawable.girl)
+//                .load("https://images.unsplash.com/photo-1698907679612-0d668523a9ea?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .centerCrop()
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+//                .circleCrop()
+                .into(mSea);
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
