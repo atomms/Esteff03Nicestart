ada027a9f5f8c54b7498e871afca978ca6a0837c
Sindy <sindy.nunez@educa.madrid.org>
2024-11-21 10:46:47 +0100

    commit

---
+++ app/src/main/java/com/example/nicestart/MainActivityToolBar.java
@@ -0,0 +1,101 @@
+package com.example.nicestart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivityToolBar extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_tool_bar);
+
+        // Establecer el Toolbar como la ActionBar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Registrar el menú contextual para un View específico (por ejemplo, textView)
+        View textView = findViewById(R.id.textView);
+        registerForContextMenu(textView);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    // Declarar e inflar el menú de opciones del Toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+
+    // Configurar la funcionalidad del menú de opciones
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.item4) {
+            final ConstraintLayout mLayout = findViewById(R.id.main);
+            Snackbar snackbar = Snackbar
+                    .make(mLayout, "Fancy a snack while you refresh?", Snackbar.LENGTH_SHORT)
+                    .setAction("Undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+            snackbar.show();
+        } else if (id == R.id.login) {
+            startActivity(new Intent(this, Login.class));
+        } else if (id == R.id.profile) {
+            startActivity(new Intent(this, Profile.class));
+        } else if (id == R.id.signup) {
+            startActivity(new Intent(this, Signup.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Declarar e inflar el menú contextual
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    // Configurar la funcionalidad del menú contextual
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.item1) {
+            Toast.makeText(this, "Item copied", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.item2) {
+            Toast.makeText(this, "Downloading item...", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+}
